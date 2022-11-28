1b105074a8e4c07cd141af3062f507db191fa96a
JustAlex322 <88882580+JustAlex322@users.noreply.github.com>
2022-11-28 17:12:19 +0300

    start dev training

---
+++ src/Routes/Routes.ts
@@ -0,0 +1,11 @@
+
+export const auth = "/auth";
+export const AuthRoutes = {
+    authRoute : auth + "/login" ,
+    registration : auth + "/registration"
+} 
+
+export const main = "/";
+export const MainRoutes = {
+    
+} 
---
+++ src/components/training/TraininigContainer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface ITrainingContainerProps {
+}
+
+const TrainingContainer: React.FunctionComponent<ITrainingContainerProps> = (props) => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default TrainingContainer;
