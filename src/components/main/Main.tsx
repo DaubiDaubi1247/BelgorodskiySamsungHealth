9bbe3d580b6d9957a065ae0ffc9166d59c91e7f0
JustAlex322 <88882580+JustAlex322@users.noreply.github.com>
2022-11-28 16:19:18 +0300

    start dev main

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IMainProps {
+}
+
+const Main: React.FunctionComponent<IMainProps> = (props) => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default Main;
