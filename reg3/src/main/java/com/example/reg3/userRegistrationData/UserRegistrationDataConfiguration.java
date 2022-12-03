c99cbdc2cc172cfcf75680451a3b5e0911839787
Александр Бахмутов <bahmytov23@gmail.com>
2022-12-03 22:39:56 +0300

    закоментил первоначальные данные

--- reg3/src/main/java/com/example/reg3/userRegistrationData/UserRegistrationDataConfiguration.java
+++ reg3/src/main/java/com/example/reg3/userRegistrationData/UserRegistrationDataConfiguration.java
@@ -1,35 +1,35 @@
-package com.example.reg3.userRegistrationData;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class UserRegistrationDataConfiguration {
-
-    @Bean
-    CommandLineRunner commandLineRunner(
-            UserRegistrationDataRepository repository) {
-        return args -> {
-            UserRegistrationData admin1 = new UserRegistrationData(
-                    "Sania S.",
-                    "admin1@gmaio.com",
-                    "admin1"
-            );
-
-            UserRegistrationData admin2 = new UserRegistrationData(
-                    "Sania B.",
-                    "admin2@gmaio.com",
-                    "admin2");
-
-                    repository.saveAll(
-                            List.of(admin1, admin2)
-            );
-
-        };
-    }
-
-
-}
+//package com.example.reg3.userRegistrationData;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class UserRegistrationDataConfiguration {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            UserRegistrationDataRepository repository) {
+//        return args -> {
+//            UserRegistrationData admin1 = new UserRegistrationData(
+//                    "Sania S.",
+//                    "admin1@gmaio.com",
+//                    "admin1"
+//            );
+//
+//            UserRegistrationData admin2 = new UserRegistrationData(
+//                    "Sania B.",
+//                    "admin2@gmaio.com",
+//                    "admin2");
+//
+//                    repository.saveAll(
+//                            List.of(admin1, admin2)
+//            );
+//
+//        };
+//    }
+//
+//
+//}
