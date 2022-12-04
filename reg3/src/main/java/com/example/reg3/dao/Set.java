88cb8fec1efb287c89f08a7845a7c10f68ddc091
Александр Бахмутов <bahmytov23@gmail.com>
2022-12-04 11:28:27 +0300

    Добавил сущность Set

---
+++ reg3/src/main/java/com/example/reg3/dao/Set.java
@@ -0,0 +1,29 @@
+package com.example.reg3.dao;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "sets")
+public class Set {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "set_id", nullable = false)
+    private Long id;
+
+    @Column(name = "number_of_repetitions", nullable = false)
+    private Integer numberOfRepetitions;
+
+
+    @Column(name = "rest_time", nullable = false)
+    @Temporal(TemporalType.TIME)
+    Date restTime;
+}
