b1238e60deeeb11fc058b1ce8aa0a099346db5b0
Александр Бахмутов <bahmytov23@gmail.com>
2022-12-04 10:49:06 +0300

    добавил сущность Trainings

---
+++ reg3/src/main/java/com/example/reg3/dao/Trainings.java
@@ -0,0 +1,26 @@
+package com.example.reg3.dao;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "trainings")
+public class Trainings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "training_id", nullable = false)
+    private Long id;
+
+    @Column(name = "label", nullable = false, length = 20, unique = true)
+    private String label;
+
+    @Column(name = "count_of_days", nullable = false)
+    private Integer countOfDays;
+}
