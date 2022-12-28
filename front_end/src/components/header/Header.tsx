d73135b2fe380885a9769e9aa99c37d77b14ab3e
JustAlex322 <88882580+JustAlex322@users.noreply.github.com>
2022-12-28 04:14:54 +0300

    ?????

--- front_end/src/components/header/Header.tsx
+++ front_end/src/components/header/Header.tsx
@@ -2,11 +2,23 @@
 import Logo from '../../common/logo/Logo';
 import styles from "./header.module.css"
 import Profile from './profile/Profile';
+import { NavLink } from 'react-router-dom';
+import { AuthRoutes } from '../../Routes/Routes';
+import { useAppDispatch } from './../../app/hooks';
+import { setAccessData, setAuth } from '../../slices/auth/authSlice';
 
 interface IHeaderProps {
 }
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+
+    const dispatch = useAppDispatch()
+
+    const exitFromAccount = () => {
+        dispatch(setAuth(false))
+        dispatch(setAccessData(null))
+    }
+
   return (
     <header className={styles.header}>
         <div className={styles.flexContainer + " mh-100 mw-100 d-flex p-2"}>
@@ -14,7 +26,10 @@ const Header: React.FunctionComponent<IHeaderProps> = (props) => {
             <div className="title">
                 Lorem, ipsum dolor.
             </div>
-            <Profile/>
+            <div className="">
+                <Profile/>
+                <NavLink to={AuthRoutes.authRoute} style={{marginLeft : "10px", color: "white", textDecoration : 'none', fontSize : '20px'}} onClick={exitFromAccount}>Выход</NavLink>
+            </div>
         </div>
     </header>
   );
